0319a40f70be136c7b300384027f2380823e9e26
Joshuwar <joshuabradley@joshuas-mbp.home>
2014-10-27 23:33:37 +0000

    adds analytics, pagination, and tagging

--- wordpress/wp-content/themes/nadia-connor/index.php
+++ wordpress/wp-content/themes/nadia-connor/index.php
@@ -9,12 +9,20 @@ get_header(); ?>
 <?php if ( have_posts() ) : ?>
 <?php while ( have_posts() ) : the_post(); ?>
 <article class="row">
-	<div class="col-sm-2">
+	<div class="col-sm-2 tags-sidebar">
 		<div class="spacer"></div>
+		<?php 
+		if ( is_single() ) {
+			the_tags('','');
+		} ?>
+		<br class="clear">
 	</div>
 	<div class="col-sm-6">
 		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h1></a>
-		<?php the_content(); ?>
+		<?php 
+		$more_link_text = '[Continue Reading]';
+		the_content($more_link_text); 
+		?>
 	</div>
 
 </article>
@@ -22,6 +30,15 @@ get_header(); ?>
 endwhile;
 endif;
 ?>
+<div class="row">
+	<div class="col-sm-6 col-sm-offset-2">
+		<?php
+		if ( function_exists('vb_pagination') ) {
+			vb_pagination();
+		}
+		?>
+	</div>	
+</div>
 
 <?php
 get_footer();
